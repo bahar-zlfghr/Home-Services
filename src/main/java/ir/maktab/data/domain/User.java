66170738624e00d19f5fa5a499e949be7c82618b
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-17 11:56:33 +0430

    move role property from user to person

--- src/main/java/ir/maktab/data/domain/User.java
+++ src/main/java/ir/maktab/data/domain/User.java
@@ -1,6 +1,5 @@
 package ir.maktab.data.domain;
 
-import ir.maktab.data.enums.PersonRole;
 import ir.maktab.data.enums.UserStatus;
 
 import javax.persistence.*;
@@ -13,9 +12,6 @@ public class User extends Person {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
-    @Enumerated(EnumType.STRING)
-    private PersonRole role;
-
     @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "account_id")
     private Account account;
@@ -26,15 +22,6 @@ public class User extends Person {
 
     public User setStatus(UserStatus status) {
         this.status = status;
-        return this;
-    }
-
-    public PersonRole getRole() {
-        return role;
-    }
-
-    public User setRole(PersonRole role) {
-        this.role = role;
         return this;
     }
 
