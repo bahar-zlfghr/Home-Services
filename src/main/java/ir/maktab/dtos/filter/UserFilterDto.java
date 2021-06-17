d800fa227eda2b9966e2e7a2c5ca4bcfc24a278f
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-17 13:22:13 +0430

    add filter options to user filter class

---
+++ src/main/java/ir/maktab/dtos/filter/UserFilterDto.java
@@ -0,0 +1,59 @@
+package ir.maktab.dtos.filter;
+
+import ir.maktab.data.enums.PersonRole;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class UserFilterDto {
+    private PersonRole role;
+    private String name;
+    private String family;
+    private String Email;
+    private String speciality;
+
+    public PersonRole getRole() {
+        return role;
+    }
+
+    public UserFilterDto setRole(PersonRole role) {
+        this.role = role;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserFilterDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public UserFilterDto setFamily(String family) {
+        this.family = family;
+        return this;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public UserFilterDto setEmail(String email) {
+        Email = email;
+        return this;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public UserFilterDto setSpeciality(String speciality) {
+        this.speciality = speciality;
+        return this;
+    }
+}
