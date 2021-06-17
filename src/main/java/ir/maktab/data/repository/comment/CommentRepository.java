927390e52eaabc5dfc00611a0a7db7e3f7711ad0
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-17 12:04:55 +0430

    create comment repository for do CRUD operation

---
+++ src/main/java/ir/maktab/data/repository/comment/CommentRepository.java
@@ -0,0 +1,28 @@
+package ir.maktab.data.repository.comment;
+
+import ir.maktab.data.domain.Comment;
+import ir.maktab.data.domain.Customer;
+import ir.maktab.data.domain.Order;
+import ir.maktab.data.domain.Specialist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    @Query("UPDATE Comment AS c SET c.score = :score WHERE c.id = :id")
+    void updateScoreComment(@Param("id") Integer id, @Param("score") Integer score);
+
+    @Query("UPDATE Comment AS c SET c.description = :description WHERE c.id = :id")
+    void updateDescriptionComment(@Param("id") Integer id, @Param("description") String description);
+
+    List<Comment> getCommentsByCustomer(Customer customer);
+    Optional<Comment> getCommentByOrder(Order order);
+    Optional<Comment> getCommentBySpecialist(Specialist specialist);
+}
