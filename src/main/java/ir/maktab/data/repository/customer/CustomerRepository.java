14827f88abe20c5a15dc83b3f0bf2baf67a29c83
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-17 12:10:40 +0430

    create customer repository for do CRUD operation

---
+++ src/main/java/ir/maktab/data/repository/customer/CustomerRepository.java
@@ -0,0 +1,28 @@
+package ir.maktab.data.repository.customer;
+
+import ir.maktab.data.domain.Customer;
+import ir.maktab.data.domain.Order;
+import ir.maktab.data.enums.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    @Query("UPDATE Customer AS c SET c.status = :status WHERE c.id = :id")
+    void updateCustomerStatus(@Param("id") Integer id, @Param("orders") UserStatus status);
+
+    @Query("UPDATE Customer AS c SET c.orders = :orders WHERE c.id = :id")
+    void updateCustomerOrders(@Param("id") Integer id, @Param("orders") List<Order> orders);
+
+    @Query("UPDATE Customer AS c SET c.password = :password WHERE c.email = :email AND c.password = :previousPassword")
+    void updateCustomerPassword(@Param("email") String email, @Param("previousPassword") String previousPassword, @Param("newPassword") String newPassword);
+
+    Optional<Customer> getCustomerByEmail(String email);
+}
