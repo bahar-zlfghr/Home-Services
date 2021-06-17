304600505240f4701aa1ae2ac2bd74be161b1fa8
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-17 12:51:45 +0430

    create specialist repository for do CRUD operation

---
+++ src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
@@ -0,0 +1,41 @@
+package ir.maktab.data.repository.specialist;
+
+import ir.maktab.data.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public interface SpecialistRepository extends JpaRepository<Specialist, Integer> {
+
+    @Query("UPDATE Specialist AS s SET s.specialty = :specialty WHERE s.id = :id")
+    void updateSpecialistSpecialty(@Param("id") Integer id, @Param("specialty") String specialty);
+
+    @Query("UPDATE Specialist AS s SET s.score = :score WHERE s.id = :id")
+    void updateSpecialistScore(@Param("id") Integer id, @Param("score") Integer score);
+
+    @Query("UPDATE Specialist AS s SET s.profilePicture = :profilePicture WHERE s.id = :id")
+    void updateSpecialistProfilePicture(@Param("id") Integer id, @Param("profilePicture") byte[] profilePicture);
+
+    @Query("UPDATE Specialist AS s SET s.subServices = :subServices WHERE s.id = :id")
+    void updateSpecialistSubServices(@Param("id") Integer id, @Param("subServices") Set<SubService> subServices);
+
+    @Query("UPDATE Specialist AS s SET s.services = :services WHERE s.id = :id")
+    void updateSpecialistServices(@Param("id") Integer id, @Param("services") Set<Service> services);
+
+    @Query("UPDATE Specialist AS s SET s.offers = :offers WHERE s.id = :id")
+    void updateSpecialistOffers(@Param("id") Integer id, @Param("offers") Set<Offer> offers);
+
+    @Query("UPDATE Specialist AS s SET s.orders = :orders WHERE s.id = :id")
+    void updateSpecialistOrders(@Param("id") Integer id, @Param("orders") Set<Order> orders);
+
+    @Query("UPDATE Specialist AS s SET s.password = :newPassword WHERE s.email = :email AND s.password = :previousPassword")
+    void updateSpecialistPassword(@Param("email") String email, @Param("previousPassword") String previousPassword, @Param("newPassword") String newPassword);
+
+    Optional<Specialist> getSpecialistByEmail(String email);
+}
