9fd99b2447bfc40c78c1a2721915a9e9f4d12dbb
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-19 13:19:47 +0430

    minor changes

--- src/main/java/ir/maktab/data/repository/offer/OfferRepository.java
+++ src/main/java/ir/maktab/data/repository/offer/OfferRepository.java
@@ -4,20 +4,16 @@ import ir.maktab.data.domain.Offer;
 import ir.maktab.data.domain.Order;
 import ir.maktab.data.domain.Specialist;
 import ir.maktab.data.enums.OfferStatus;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.Set;
 
 /**
  * @author : Bahar Zolfaghari
  **/
-public interface OfferRepository extends JpaRepository<Offer, Integer>, JpaSpecificationExecutor<Offer> {
+public interface OfferRepository extends JpaRepository<Offer, Integer> {
 
     @Query("UPDATE Offer AS o SET o.suggestedPrice = :suggestedPrice WHERE o.id = :id")
     void updateOfferSuggestedPrice(@Param("id") Integer id, @Param("suggestedPrice") Long suggestedPrice);
@@ -26,25 +22,5 @@ public interface OfferRepository extends JpaRepository<Offer, Integer>, JpaSpeci
     void updateOfferStatus(@Param("id") Integer id, @Param("status") OfferStatus offerStatus);
 
     Set<Offer> getOffersBySpecialist(Specialist specialist);
-
     Set<Offer> getOffersByOrder(Order order);
-
-    static Specification<Offer> getSortedOffersBySuggestedPriceOrScore(boolean suggestedPrice, boolean score) {
-        return (Specification<Offer>) (root, query, criteriaBuilder) -> {
-
-            CriteriaQuery<Offer> criteriaQuery = criteriaBuilder.createQuery(Offer.class);
-
-            Root<Offer> from = criteriaQuery.from(Offer.class);
-            CriteriaQuery<Offer> select = criteriaQuery.select(from);
-
-            if (suggestedPrice) {
-                criteriaQuery.orderBy(criteriaBuilder.asc(from.get("suggestedPrice")));
-            }
-            if (score) {
-                criteriaQuery.orderBy(criteriaBuilder.asc(from.get("specialist").get("score")));
-            }
-
-            return criteriaQuery.getRestriction();
-        };
-    }
 }
