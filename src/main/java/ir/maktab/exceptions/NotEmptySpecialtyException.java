45d601d4b2fa171f616c3137ca064d72a9ccbc2e
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-19 19:57:42 +0430

    add profile picture and specialty exception handling

---
+++ src/main/java/ir/maktab/exceptions/NotEmptySpecialtyException.java
@@ -0,0 +1,10 @@
+package ir.maktab.exceptions;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class NotEmptySpecialtyException extends Exception {
+    public NotEmptySpecialtyException(String message) {
+        super(message);
+    }
+}
