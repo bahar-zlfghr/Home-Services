6dbe893dad97ca1f38c87d0506f13fcf54b0960c
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-19 13:37:48 +0430

    create spring data configuration

---
+++ src/main/java/ir/maktab/configuration/SpringDataConfiguration.java
@@ -0,0 +1,74 @@
+package ir.maktab.configuration;
+
+import ir.maktab.configuration.properties.DatabaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "ir.maktab.data.repository")
+public class SpringDataConfiguration {
+    private final DatabaseProperties databaseProperties;
+
+    public SpringDataConfiguration(DatabaseProperties databaseProperties) {
+        this.databaseProperties = databaseProperties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(databaseProperties.getDriverClass());
+        dataSource.setUrl(databaseProperties.getUrl());
+        dataSource.setUsername(databaseProperties.getUsername());
+        dataSource.setPassword(databaseProperties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("ir.maktab.data.domain");
+
+        Properties jpaProperties = new Properties() {
+            {
+                put("hibernate.hbm2ddl.auto",
+                        databaseProperties.getHbm2ddl());
+                put("hibernate.show_sql",
+                        databaseProperties.getShowSql());
+                put("hibernate.format_sql",
+                        databaseProperties.getFormatSql());
+            }
+        };
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return transactionManager;
+    }
+}
