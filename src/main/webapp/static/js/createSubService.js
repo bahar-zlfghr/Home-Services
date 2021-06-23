c20071082553103a395d5d814a8999d498b83a53
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-23 16:48:23 +0430

    minor changes

--- src/main/webapp/static/js/createSubService.js
+++ src/main/webapp/static/js/createSubService.js
@@ -3,7 +3,6 @@ function validateForm() {
 }
 
 function validateService() {
-    var serviceName = document.getElementById('serviceName').value;
     console.log("service name: ", serviceName);
     if (serviceName.length === 0) {
         document.getElementById('serviceError').innerHTML = 'Choose one service';
@@ -17,7 +16,6 @@ function validateService() {
 
 function validateName() {
     var subServiceName = document.getElementById('subServiceName').value;
-    console.log("sub service name: ", subServiceName);
     if (subServiceName.length < 5 || subServiceName.length > 30) {
         document.getElementById('subServiceNameError').innerHTML = 'Sub service name should be between 4 and 30 characters long';
         return false;
@@ -29,7 +27,6 @@ function validateName() {
 
 function validateBasePrice() {
     var basePrice = document.getElementById('basePrice').value;
-    console.log("base price: ", basePrice);
     if (basePrice.length === 0) {
         document.getElementById('basePriceError').innerHTML = 'Base price is required';
         return false;
