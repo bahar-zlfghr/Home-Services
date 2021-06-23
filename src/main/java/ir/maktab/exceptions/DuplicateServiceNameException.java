fb69291a60666be03dc0ab45d2bafe16760fbfb1
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-23 10:49:48 +0430

    create this class for handle duplicate service name exception

---
+++ src/main/java/ir/maktab/exceptions/DuplicateServiceNameException.java
@@ -0,0 +1,11 @@
+package ir.maktab.exceptions;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class DuplicateServiceNameException extends Exception {
+
+    public DuplicateServiceNameException(String message) {
+        super(message);
+    }
+}
