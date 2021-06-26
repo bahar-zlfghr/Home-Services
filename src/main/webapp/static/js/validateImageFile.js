ad57c5bda27d20f3a2c37688c1c5382f28af9f4b
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-26 16:41:36 +0430

    create validate upload file by specialist

--- src/main/webapp/static/js/validateImageFile.js
+++ src/main/webapp/static/js/validateImageFile.js
@@ -2,50 +2,33 @@ const maxAllowedSize = 300 * 1024;
 const allowedExtension = ["jpg"];
 
 function checkFormatAndSizeProfilePicture () {
-    var role = document.getElementById('role').value;
-
-    if (role === "specialist") {
-        var profilePicture = document.getElementById('profilePicture');
-
+    var profilePicture = document.getElementById('profilePicture');
+    if (profilePicture === null) {
+        document.getElementById('profilePictureError').innerHTML = 'Choose a .jpg file';
+        return false;
+    }
+    else {
+        document.getElementById('profilePictureError').innerHTML = '';
         const {name: profilePictureName, size: profilePictureSize} = profilePicture.files[0];
         const profilePictureExtension = profilePictureName.split('.').pop();
-
         if (allowedExtension.includes(profilePictureExtension)) {
             if (profilePictureSize > maxAllowedSize) {
                 document.getElementById('profilePicturePreview').innerHTML = '';
-
                 document.getElementById('profilePictureError').innerHTML = 'Picture size is too large!';
                 document.getElementById('profilePicturePreview').innerHTML = '';
-
                 return false;
-            }
-            else {
+            } else {
                 var reader = new FileReader();
-                reader.onload = function(e) {
+                reader.onload = function (e) {
                     document.getElementById('profilePicturePreview').innerHTML = '<img src="' + e.target.result + '" alt="Oops!"/>';
                     document.getElementById('profilePictureError').innerHTML = '';
                 };
                 reader.readAsDataURL(profilePicture.files[0]);
             }
-        }
-        else {
+        } else {
             document.getElementById('profilePictureError').innerHTML = 'Invalid picture type';
             document.getElementById('profilePicturePreview').innerHTML = '';
             return false;
         }
     }
 }
-
-$(document).ready(function(){
-    $("select").change(function(){
-        $(this).find("option:selected").each(function(){
-            var optionValue = $(this).attr("value");
-            if(optionValue){
-                $(".box").not("." + optionValue).hide();
-                $("." + optionValue).show();
-            } else{
-                $(".box").hide();
-            }
-        });
-    }).change();
-});
