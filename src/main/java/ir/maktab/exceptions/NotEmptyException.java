8b77245b9010d9bd0f4ff7963cbe5aba395970a2
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-26 16:24:13 +0430

    create this class for handle not empty exceptions

---
+++ src/main/java/ir/maktab/exceptions/NotEmptyException.java
@@ -0,0 +1,10 @@
+package ir.maktab.exceptions;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class NotEmptyException extends Exception {
+    public NotEmptyException(String message) {
+        super(message);
+    }
+}
