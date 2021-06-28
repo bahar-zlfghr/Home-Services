0418394d9b280fbc2fcbd385f41894d15f1357c2
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-28 18:48:23 +0430

    create this class for handle sub service already provided by specialist

---
+++ src/main/java/ir/maktab/exceptions/SubServiceAlreadyProvidedBySpecialistException.java
@@ -0,0 +1,10 @@
+package ir.maktab.exceptions;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class SubServiceAlreadyProvidedBySpecialistException extends Exception {
+    public SubServiceAlreadyProvidedBySpecialistException(String message) {
+        super(message);
+    }
+}
