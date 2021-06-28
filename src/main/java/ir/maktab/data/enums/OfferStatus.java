f45fc188a104e5c4e18d279f5a35b20178b30d4a
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-28 18:44:27 +0430

    add registered status for offer

--- src/main/java/ir/maktab/data/enums/OfferStatus.java
+++ src/main/java/ir/maktab/data/enums/OfferStatus.java
@@ -4,6 +4,7 @@ package ir.maktab.data.enums;
  * @author : Bahar Zolfaghari
  **/
 public enum OfferStatus {
+    REGISTERED("registered"),
     ACCEPTED("accepted"),
     NOT_ACCEPTED("not accepted");
 
