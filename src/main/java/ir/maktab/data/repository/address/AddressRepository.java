1c20381940bfa454a57d21e07a61d70789cca73d
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-28 18:44:42 +0430

    fix bug

--- src/main/java/ir/maktab/data/repository/address/AddressRepository.java
+++ src/main/java/ir/maktab/data/repository/address/AddressRepository.java
@@ -20,8 +20,8 @@ public interface AddressRepository extends JpaRepository<Address, Integer> {
     @Query("UPDATE Address AS a SET a.state = :state WHERE a.id = :id")
     void updateStateAddress(@Param("id") Integer id, @Param("state") String state);
 
-    @Query("UPDATE Address AS a SET a.formatted_address = :formatted_address WHERE a.id = :id")
-    void updateAlleyAddress(@Param("id") Integer id, @Param("formatted_address") String formatted_address);
+    @Query("UPDATE Address AS a SET a.formattedAddress = :formatted_address WHERE a.id = :id")
+    void updateAddress(@Param("id") Integer id, @Param("formatted_address") String formatted_address);
 
     Set<Address> getAddressesByCity(String city);
     Set<Address> getAddressesByState(String state);
