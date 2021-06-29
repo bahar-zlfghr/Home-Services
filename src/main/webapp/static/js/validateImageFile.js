ed50545e631a8264cbb1f6585e79736b7a7c0538
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-06-29 16:53:59 +0430

    check size and format specialist profile picture

--- src/main/webapp/static/js/validateImageFile.js
+++ src/main/webapp/static/js/validateImageFile.js
@@ -1,34 +1,29 @@
 const maxAllowedSize = 300 * 1024;
 const allowedExtension = ["jpg"];
 
-function checkFormatAndSizeProfilePicture () {
-    var profilePicture = document.getElementById('profilePicture');
-    if (profilePicture === null) {
-        document.getElementById('profilePictureError').innerHTML = 'Choose a .jpg file';
-        return false;
-    }
-    else {
-        document.getElementById('profilePictureError').innerHTML = '';
-        const {name: profilePictureName, size: profilePictureSize} = profilePicture.files[0];
-        const profilePictureExtension = profilePictureName.split('.').pop();
-        if (allowedExtension.includes(profilePictureExtension)) {
-            if (profilePictureSize > maxAllowedSize) {
-                document.getElementById('profilePicturePreview').innerHTML = '';
-                document.getElementById('profilePictureError').innerHTML = 'Picture size is too large!';
-                document.getElementById('profilePicturePreview').innerHTML = '';
-                return false;
-            } else {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    document.getElementById('profilePicturePreview').innerHTML = '<img src="' + e.target.result + '" alt="Oops!"/>';
-                    document.getElementById('profilePictureError').innerHTML = '';
-                };
-                reader.readAsDataURL(profilePicture.files[0]);
-            }
-        } else {
-            document.getElementById('profilePictureError').innerHTML = 'Invalid picture type';
-            document.getElementById('profilePicturePreview').innerHTML = '';
+function checkFormatAndSizeProfilePicture() {
+    var profilePicture = document.getElementById('picture');
+    document.getElementById('pictureError').innerHTML = '';
+    const {name: profilePictureName, size: profilePictureSize} = profilePicture.files[0];
+    const profilePictureExtension = profilePictureName.split('.').pop();
+    if (allowedExtension.includes(profilePictureExtension)) {
+        if (profilePictureSize > maxAllowedSize) {
+            document.getElementById('picturePreview').innerHTML = '';
+            document.getElementById('pictureError').innerHTML = 'Picture size is too large!';
+            document.getElementById('picturePreview').innerHTML = '';
             return false;
+        } else {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById('picturePreview').innerHTML = '<img src="' + e.target.result + '" alt="Oops!"' + ' style="' + 'width: 195px; height: 195px"' + '/>';
+                document.getElementById('pictureError').innerHTML = '';
+                document.getElementById('pictureEmptyError').innerHTML = '';
+            };
+            reader.readAsDataURL(profilePicture.files[0]);
         }
+    } else {
+        document.getElementById('pictureError').innerHTML = 'Invalid picture type';
+        document.getElementById('picturePreview').innerHTML = '';
+        return false;
     }
 }
