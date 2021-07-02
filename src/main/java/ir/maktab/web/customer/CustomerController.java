52fe0edb98f98e6e27aa55ab1062d557357bcbd3
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-07-02 08:23:15 +0430

    minor changes

--- src/main/java/ir/maktab/web/customer/CustomerController.java
+++ src/main/java/ir/maktab/web/customer/CustomerController.java
@@ -1,15 +1,24 @@
 package ir.maktab.web.customer;
 
 import ir.maktab.configuration.LastViewInterceptor;
-import ir.maktab.data.enums.PersonRole;
-import ir.maktab.data.enums.UserStatus;
 import ir.maktab.dtos.*;
-import ir.maktab.dtos.factory.AccountFactory;
+import ir.maktab.dtos.creator.AddressCreator;
+import ir.maktab.dtos.creator.CustomerCreator;
+import ir.maktab.dtos.creator.MailCreator;
+import ir.maktab.dtos.creator.OrderCreator;
 import ir.maktab.exceptions.DuplicateEmailException;
+import ir.maktab.exceptions.InvalidSuggestedPriceException;
+import ir.maktab.exceptions.NotFoundSubServiceException;
 import ir.maktab.exceptions.NotFoundUserException;
 import ir.maktab.service.customer.CustomerService;
-import ir.maktab.validationgroup.LoginGroup;
+import ir.maktab.service.mail.EmailSenderService;
+import ir.maktab.service.order.OrderService;
+import ir.maktab.service.service.ServiceService;
+import ir.maktab.service.subservice.SubServiceService;
+import ir.maktab.service.token.ConfirmationTokenService;
 import ir.maktab.validationgroup.RegistrationGroup;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
@@ -18,6 +27,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,58 +39,77 @@ import java.util.Map;
 @RequestMapping("/customer")
 public class CustomerController {
     private final CustomerService customerService;
+    private final SubServiceService subServiceService;
+    private final ServiceService serviceService;
+    private final OrderService orderService;
+    private final EmailSenderService emailSenderService;
+    private final ConfirmationTokenService confirmationTokenService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, SubServiceService subServiceService, ServiceService serviceService,
+                              OrderService orderService, EmailSenderService emailSenderService, ConfirmationTokenService confirmationTokenService) {
         this.customerService = customerService;
+        this.subServiceService = subServiceService;
+        this.serviceService = serviceService;
+        this.orderService = orderService;
+        this.emailSenderService = emailSenderService;
+        this.confirmationTokenService = confirmationTokenService;
     }
 
-    @GetMapping("/registration")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @GetMapping("")
+    public String customer() {
+        return "/customer/customer";
+    }
+
+    @GetMapping("/register")
     public ModelAndView registrationForm() {
-        return new ModelAndView(
-                "/customer/registration",
-                "customerDto", new CustomerDto());
-    }
-
-    @PostMapping("/registration")
-    public String registerCustomer(@ModelAttribute("customerDto") @Validated(RegistrationGroup.class) CustomerDto customerDto,
-                                   Model model) throws DuplicateEmailException {
-        customerService.checkDuplicateEmail(customerDto.getEmail());
-        AccountDto accountDto = AccountFactory.createAccount();
-        customerDto.setAccountDto(accountDto);
-        customerDto.setRole(PersonRole.CUSTOMER);
-        customerDto.setStatus(UserStatus.NEW);
-        customerService.saveCustomer(customerDto);
-        model.addAttribute("customerDto", customerDto);
-        return "/customer/registerSuccess";
-    }
-
-    @GetMapping("/login")
-    public ModelAndView loginForm() {
-        return new ModelAndView(
-                "/customer/login",
-                "customerDto", new CustomerDto());
-    }
-
-    @PostMapping("/login")
-    public String loginCustomer(@ModelAttribute("customerDto") @Validated(LoginGroup.class) CustomerDto customerDto,
-                                Model model) throws NotFoundUserException {
-        CustomerDto customerByEmailAndPassword = customerService.getCustomerByEmailAndPassword(customerDto.getEmail(), customerDto.getPassword());
-        model.addAttribute("customerDto", customerByEmailAndPassword);
-        return "/customer/panel";
-    }
-
-    @GetMapping("/logout")
-    public ModelAndView logoutButton() {
-        ModelAndView modelAndView = new ModelAndView(
-                "/customer/login",
-                "customerDto", new CustomerDto());
-        modelAndView.addObject("logout", true);
+        ModelAndView modelAndView = new ModelAndView("/customer/registration");
+        modelAndView.addObject("customerDto", new CustomerDto());
+        modelAndView.addObject("successRegister", false);
         return modelAndView;
     }
 
-    @GetMapping("/panel")
-    public String customerPanel() {
-        return "/customer/panel";
+    @PostMapping("/register")
+    public String registerCustomer(@ModelAttribute("customerDto") @Validated(RegistrationGroup.class) CustomerDto customerDto,
+                                   Model model) throws DuplicateEmailException {
+        CustomerDto customer = CustomerCreator.createCustomerDto(customerDto);
+        customerService.saveCustomer(customer);
+        emailSenderService.sendEmail(MailCreator.createEmail(customerDto, customer.getConfirmationTokenDto()));
+        model.addAttribute("customerEmail", customerDto.getEmail());
+        model.addAttribute("successRegister", true);
+        model.addAttribute("customerDto", new CustomerDto());
+        return "/customer/registration";
+    }
+
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @GetMapping("/create-order")
+    public ModelAndView createOrderForm() {
+        ModelAndView modelAndView = new ModelAndView("/customer/createOrder");
+        modelAndView.addObject("successCreatOrder", false);
+        modelAndView.addObject("subServices", subServiceService.getAllSubServices());
+        return modelAndView;
+    }
+
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @PostMapping("/create-order")
+    public String createOrder(@RequestParam("subServiceName") String subServiceName,
+                              @RequestParam("suggestedPrice") String suggestedPrice,
+                              @RequestParam("description") String description,
+                              @RequestParam("doDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date doDate,
+                              @RequestParam("state") String state,
+                              @RequestParam("city") String city,
+                              @RequestParam("formatted_address") String formattedAddress,
+                              Principal principal,
+                              Model model) throws NotFoundSubServiceException, NotFoundUserException, InvalidSuggestedPriceException {
+        CustomerDto customerDto = customerService.getCustomerByEmail(principal.getName());
+        SubServiceDto subServiceDto = subServiceService.getSubServiceByName(subServiceName);
+        AddressDto addressDto = AddressCreator.createAddress(state, city, formattedAddress);
+        OrderDto orderDto = OrderCreator.createOrder(customerDto, subServiceDto, suggestedPrice, description, doDate, addressDto);
+        orderService.saveOrder(orderDto);
+        model.addAttribute("successCreatOrder", true);
+        model.addAttribute("subServices", subServiceService.getAllSubServices());
+        model.addAttribute("customerDto", new CustomerDto());
+        return "/customer/createOrder";
     }
 
     @ExceptionHandler(value = BindException.class)
@@ -97,11 +127,13 @@ public class CustomerController {
         return new ModelAndView(lastView, model);
     }
 
-    @ExceptionHandler(value = NotFoundUserException.class)
-    public ModelAndView notFoundUserExceptionHandler(NotFoundUserException ex, HttpServletRequest request) {
+    @ExceptionHandler(value = InvalidSuggestedPriceException.class)
+    public ModelAndView invalidSuggestedPriceExceptionHandler(InvalidSuggestedPriceException ex, HttpServletRequest request) {
         String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
         Map<String, Object> model = new HashMap<>();
-        model.put("errorLogin", ex.getMessage());
+        model.put("successCreatOrder", false);
+        model.put("invalidSuggestedPrice", ex.getMessage());
+        model.put("subServices", subServiceService.getAllSubServices());
         model.put("customerDto", new CustomerDto());
         return new ModelAndView(lastView, model);
     }
--- src/main/java/ir/maktab/web/manager/ManagerController.java
+++ src/main/java/ir/maktab/web/manager/ManagerController.java
@@ -1,10 +1,8 @@
 package ir.maktab.web.manager;
 
 import ir.maktab.configuration.LastViewInterceptor;
-import ir.maktab.dtos.ServiceDto;
-import ir.maktab.dtos.SpecialistDto;
-import ir.maktab.dtos.SubServiceDto;
-import ir.maktab.dtos.factory.SubserviceFactory;
+import ir.maktab.dtos.*;
+import ir.maktab.dtos.creator.SubserviceCreator;
 import ir.maktab.dtos.filter.UserFilterDto;
 import ir.maktab.dtos.filter.UserFilterResult;
 import ir.maktab.exceptions.*;
@@ -13,6 +11,7 @@ import ir.maktab.service.service.ServiceService;
 import ir.maktab.service.specialist.SpecialistService;
 import ir.maktab.service.subservice.SubServiceService;
 import ir.maktab.service.user.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindException;
@@ -44,78 +43,119 @@ public class ManagerController {
         this.specialistService = specialistService;
     }
 
-    @GetMapping("/filter")
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("")
+    public String manager() {
+        return "/manager/manager";
+    }
+
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/filter-users")
     public ModelAndView filterUsersForm() {
         return new ModelAndView(
                 "/manager/filterUser",
                 "userFilterDto", new UserFilterDto());
     }
 
-    @PostMapping("/filter")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping("/filter-users")
     public ModelAndView filterUsers(@ModelAttribute("userFilterDto") UserFilterDto userFilterDto) {
-        //ToDo: add subservice property
-        //ToDo: pagination
         UserFilterResult result = userService.filterUsers(userFilterDto);
         return new ModelAndView(
                 "/manager/filterUser",
                 "users", result);
     }
 
-    @GetMapping("/create/service")
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/create-service")
     public ModelAndView createServiceForm() {
-        return new ModelAndView(
-                "/manager/createService",
-                "serviceDto", new ServiceDto());
+        ModelAndView modelAndView = new ModelAndView(
+                "/manager/createService");
+        modelAndView.addObject("serviceDto", new ServiceDto());
+        modelAndView.addObject("successCreateService", false);
+        return modelAndView;
     }
 
-    @PostMapping("/create/service")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping("/create-service")
     public String createService(@ModelAttribute("serviceDto") @Valid ServiceDto serviceDto,
                                 Model model) throws DuplicateServiceNameException {
         serviceService.saveService(serviceDto);
-        model.addAttribute("success", true);
+        model.addAttribute("successCreateService", true);
+        model.addAttribute("serviceDto", new ServiceDto());
         return "/manager/createService";
     }
 
-    @GetMapping("/create/subservice")
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/create-sub-service")
     public ModelAndView createSubServiceForm() {
-        return new ModelAndView(
-                "/manager/createSubService",
-                "services", serviceService.getAllServices());
+        ModelAndView modelAndView = new ModelAndView("/manager/createSubService");
+        modelAndView.addObject("services", serviceService.getAllServices());
+        modelAndView.addObject("successCreateSubService", false);
+        return modelAndView;
     }
 
-    @PostMapping("/create/subservice")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping("/create-sub-service")
     public String createService(@RequestParam(value = "serviceName") String serviceName,
                                 @RequestParam(value = "subServiceName") String subServiceName,
                                 @RequestParam(value = "basePrice") String basePrice,
                                 @RequestParam(value = "description") String description,
                                 Model model) throws DuplicateSubServiceNameException, NotFoundServiceException {
         ServiceDto serviceDto = serviceService.getServiceByName(serviceName);
-        SubServiceDto subServiceDto = SubserviceFactory.createSubService(serviceDto, subServiceName, basePrice, description);
+        SubServiceDto subServiceDto = SubserviceCreator.createSubService(serviceDto, subServiceName, basePrice, description);
         subServiceService.saveSubService(subServiceDto);
-        model.addAttribute("success", true);
+        model.addAttribute("successCreateSubService", true);
         model.addAttribute("services", serviceService.getAllServices());
         return "/manager/createSubService";
     }
 
-    @GetMapping("/add/specialist/subservice")
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/add-specialist-sub-service")
     public ModelAndView assignSpecialistToSubServiceForm() {
         ModelAndView modelAndView = new ModelAndView("/manager/assignSpecialistToSubService");
+        modelAndView.addObject("successAssignSpecialistToSubService", false);
         modelAndView.getModel().put("subServices", subServiceService.getAllSubServices());
         modelAndView.getModel().put("specialists", specialistService.getAllSpecialists());
         return modelAndView;
     }
 
-    @PostMapping("/add/specialist/subservice")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping("/add-specialist-sub-service")
     public String assignSpecialistToSubServiceForm(@RequestParam(value = "subServiceName") String subServiceName,
                                                    @RequestParam(value = "specialistEmail") String specialistEmail,
                                                    Model model) throws NotFoundSubServiceException, NotFoundUserException, SubServiceAlreadyProvidedBySpecialistException {
         SubServiceDto subServiceDto = subServiceService.getSubServiceByName(subServiceName);
         SpecialistDto specialistDto = specialistService.getSpecialistByEmail(specialistEmail);
         subServiceService.updateSubServiceSpecialists(subServiceDto, specialistDto);
-        model.addAttribute("success", true);
+        model.addAttribute("successAssignSpecialistToSubService", true);
         model.addAttribute("subServices", subServiceService.getAllSubServices());
         model.addAttribute("specialists", specialistService.getAllSpecialists());
         return "/manager/assignSpecialistToSubService";
+    }
+
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/add-specialist-service")
+    public ModelAndView assignSpecialistToServiceForm() {
+        ModelAndView modelAndView = new ModelAndView("/manager/assignSpecialistToService");
+        modelAndView.addObject("successAssignSpecialistToService", false);
+        modelAndView.getModel().put("services", serviceService.getAllServices());
+        modelAndView.getModel().put("specialists", specialistService.getAllSpecialists());
+        return modelAndView;
+    }
+
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping("/add-specialist-service")
+    public String assignSpecialistToServiceForm(@RequestParam(value = "serviceName") String serviceName,
+                                                @RequestParam(value = "specialistEmail") String specialistEmail,
+                                                Model model) throws NotFoundServiceException, NotFoundUserException, ServiceAlreadyProvidedBySpecialistException {
+        ServiceDto serviceDto = serviceService.getServiceByName(serviceName);
+        SpecialistDto specialistDto = specialistService.getSpecialistByEmail(specialistEmail);
+        serviceService.updateServiceSpecialists(serviceDto, specialistDto);
+        model.addAttribute("successAssignSpecialistToService", true);
+        model.addAttribute("services", subServiceService.getAllSubServices());
+        model.addAttribute("specialists", specialistService.getAllSpecialists());
+        return "/manager/assignSpecialistToService";
     }
 
     @ExceptionHandler(value = BindException.class)
@@ -128,6 +168,7 @@ public class ManagerController {
     public ModelAndView duplicateServiceNameExceptionHandler(DuplicateServiceNameException ex, HttpServletRequest request) {
         String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
         Map<String, Object> model = new HashMap<>();
+        model.put("successCreateService", false);
         model.put("duplicateServiceName", ex.getMessage());
         model.put("services", serviceService.getAllServices());
         model.put("serviceDto", new ServiceDto());
@@ -138,6 +179,7 @@ public class ManagerController {
     public ModelAndView duplicateSubServiceNameExceptionHandler(DuplicateSubServiceNameException ex, HttpServletRequest request) {
         String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
         Map<String, Object> model = new HashMap<>();
+        model.put("successCreateSubService", false);
         model.put("duplicateSubServiceName", ex.getMessage());
         model.put("subServices", subServiceService.getAllSubServices());
         model.put("specialists", specialistService.getAllSpecialists());
@@ -149,10 +191,33 @@ public class ManagerController {
     public ModelAndView subServiceAlreadyProvidedBySpecialistExceptionHandler(SubServiceAlreadyProvidedBySpecialistException ex, HttpServletRequest request) {
         String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
         Map<String, Object> model = new HashMap<>();
+        model.put("successAssignSpecialistToSubService", false);
         model.put("subServiceAlreadyProvidedBySpecialist", ex.getMessage());
         model.put("subServices", subServiceService.getAllSubServices());
         model.put("specialists", specialistService.getAllSpecialists());
         model.put("subServiceDto", new SubServiceDto());
+        return new ModelAndView(lastView, model);
+    }
+
+    @ExceptionHandler(value = ServiceAlreadyProvidedBySpecialistException.class)
+    public ModelAndView serviceAlreadyProvidedBySpecialistExceptionHandler(ServiceAlreadyProvidedBySpecialistException ex, HttpServletRequest request) {
+        String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
+        Map<String, Object> model = new HashMap<>();
+        model.put("successAssignSpecialistToService", false);
+        model.put("serviceAlreadyProvidedBySpecialist", ex.getMessage());
+        model.put("services", serviceService.getAllServices());
+        model.put("specialists", specialistService.getAllSpecialists());
+        model.put("serviceDto", new SubServiceDto());
+        return new ModelAndView(lastView, model);
+    }
+
+    @ExceptionHandler(value = NotFoundUserException.class)
+    public ModelAndView notFoundUserExceptionHandler(NotFoundUserException ex, HttpServletRequest request) {
+        String lastView = (String) request.getSession().getAttribute(LastViewInterceptor.LAST_VIEW_ATTRIBUTE);
+        Map<String, Object> model = new HashMap<>();
+        model.put("loginError", ex.getMessage());
+        model.put("managerDto", new ManagerDto());
+        request.getSession().setAttribute("fail", true);
         return new ModelAndView(lastView, model);
     }
 }
